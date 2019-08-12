4b8ea1e725ec50e9ddb48a4d170882f5b4e78b7f
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-12 15:20:28 +0530

    Main.java: add printing table functionality

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,16 @@
  * */
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a no");
+        int n = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%dx%d = %d\n", n, i, n * i);
+        }
     }
 
 }
