5650c879a2e766e735de5ebc01f999b743022517
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-15 22:56:43 +0530

    Main.java: adding even or odd

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,11 +9,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a no");
-        int n = input.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            System.out.printf("%dx%d = %d\n", n, i, n * i);
+//        System.out.println("Enter a no");
+//        int n = input.nextInt();
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.printf("%dx%d = %d\n", n, i, n * i);
+//        }
+//    }
+        //Even Odd
+        int num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
         }
     }
-
 }
