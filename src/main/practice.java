7a0e709e5cd2127cc063ab48923848864574270c
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-18 18:44:13 +0530

    initial commit practice.java

---
+++ src/main/practice.java
@@ -0,0 +1,11 @@
+package main;
+//Write a program in Java to accept a String value from the user and print a welcome message in the format given below.
+
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
