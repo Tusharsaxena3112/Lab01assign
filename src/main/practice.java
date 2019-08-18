9d6bc1c69dce3d5d16792750c4ddf05b6cfb88ad
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-18 19:06:59 +0530

    practice1.java:adding ques3

--- src/main/practice.java
+++ src/main/practice.java
@@ -6,6 +6,7 @@ public class practice {
         practice1 pobj = new practice1();
         //pobj.ques1();  //Write a program in Java to accept a String value from the user and print a welcome message in the format given below.
         //pobj.ques2();  //Write a program in Java that accepts two String type values and generate the output in the required format.
+        //pobj.ques3();  //Write a program in Java to accept two integers and print the following -
     }
 }
 
--- src/main/practice1.java
+++ src/main/practice1.java
@@ -17,5 +17,21 @@ public class practice1 {
         System.out.printf("%s University %s ", s1, s2);
 
     }
+//    12 + 3 = 15
+//            12 - 3 = 9
+//            12 * 3 = 36
+//            12 / 3 = 4
+//            12 % 3 = 0
+
+    public void ques3() {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+        System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+        System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+        System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+        System.out.printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+    }
 
 }
