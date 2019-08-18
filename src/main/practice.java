ffd38c37e682e74ba9a6d2242723f0de283cfa5c
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-18 18:47:24 +0530

    practice.java:adding code

--- src/main/practice.java
+++ src/main/practice.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class practice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.printf("Welcome, %s!", name);
 
     }
 }
