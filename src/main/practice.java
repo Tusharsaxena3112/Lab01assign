e1b782ac782c8b2096498523d5bccbe00b484f01
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-18 18:54:39 +0530

    initial commit practice1.java

--- src/main/practice.java
+++ src/main/practice.java
@@ -1,13 +1,11 @@
 package main;
 //Write a program in Java to accept a String value from the user and print a welcome message in the format given below.
 
-import java.util.Scanner;
-
 public class practice {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
-        System.out.printf("Welcome, %s!", name);
-
+        practice1 pobj = new practice1();
+        //pobj.ques1();
+        //
     }
 }
+
---
+++ src/main/practice1.java
@@ -0,0 +1,13 @@
+package main;
+//Write a program in Java that accepts two String type values and generate the output in the required format.
+
+import java.util.Scanner;
+
+public class practice1 {
+    public void ques1() {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.printf("Welcome, %s!", name);
+    }
+
+}
